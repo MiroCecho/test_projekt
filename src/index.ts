0b9aecc1900f6ef468ba17a0ef1c0c845ac8f4cb
Miro Cecho <miro.cecho@gmail.com>
2023-01-17 07:44:11 +0100

    fetch data.json

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-const btTest=document.getElementById("btTest");
+const btTest = document.getElementById("btTest");
 
-btTest?.addEventListener("click",()=>{
-  alert("ahoj")
+btTest?.addEventListener("click", () => {
+  fetch("http://localhost:3000/drawing/1")
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+    }
+    );
 })
